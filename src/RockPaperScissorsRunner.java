032953ada7ba6e9d7766181fba5221896384f02c
J14P <jackson_pinchot@yahoo.com>
2022-09-28 16:29:09 -0400

    Complete code for Lab 05 Rock Paper Scissors

---
+++ src/RockPaperScissorsRunner.java
@@ -0,0 +1,5 @@
+public class RockPaperScissorsRunner extends RockPaperScissorsFrame {
+    public static void main(String[] args) {
+        RockPaperScissorsFrame frame = new RockPaperScissorsFrame();
+    }
+}
